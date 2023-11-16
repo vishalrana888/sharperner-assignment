cd56a55f91411e4c00103981e0117a5e003296db
vishalrana888 <vishalrana888@gmail.com>
2023-11-16 17:32:54 +0530

    QuerySelector && QuerySelectorALL

---
+++ task4.js
@@ -0,0 +1,41 @@
+// QuerySelector
+
+//  var header = document.querySelector('#main-header');
+//  header.style.borderbottom = 'solid 4px #ccc';
+
+//  var input = document.querySelector('input');
+//  input.value = 'Hello world'
+
+//  var submit = document.querySelector('input[type="submit"]');
+//  submit.value = "SEND"
+
+//  var item = document.querySelector('.list-group-item');
+//  item.style.color = 'red' ;
+
+//  var lastitem = document.querySelector('.list-group-item:last-child');
+//  lastitem.style.color = 'orange' ;
+
+// TASK IN ASSSIGNMENT
+
+//  var seconditem = document.querySelector('.list-group-item:nth-child(2)');
+//  seconditem.style.color = 'green' ;
+
+//  var thirditem = document.querySelector('.list-group-item:nth-child(3)');
+//  thirditem.style.display = 'none' ;
+
+
+ //QuerySelectorALL
+
+//  var titles = document.querySelectorAll('.title');
+// console.log(titles);
+
+
+// TASK IN ASSIGNMENT
+
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// for (var i=0; i<odd.length;i++)
+// {
+//    odd[i].style.backgroundColor ='#00FF00'
+// }
+// var item = document.querySelectorAll('#items li');
+// item[1].style.color = 'green';
