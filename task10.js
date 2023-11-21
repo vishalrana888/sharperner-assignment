7bc163de8a2b78d12ca8c8deed1009447d572d6c
vishalrana888 <vishalrana888@gmail.com>
2023-11-21 11:50:19 +0530

    Delete data from Local Storage

---
+++ task10.js
@@ -0,0 +1,64 @@
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents the form from submitting and refreshing the page
+
+    // Get user input
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+
+    // Create an object with user details
+    var userDetails = {
+      username: username,
+      email: email,
+      phone: phone
+    };
+
+    // Retrieve existing user data from local storage (if any)
+    var existingUsers = JSON.parse(localStorage.getItem('users')) || [];
+
+    // Add new user details to the existing array
+    existingUsers.push(userDetails);
+
+    // Store the updated user details array in local storage
+    localStorage.setItem('users', JSON.stringify(existingUsers));
+    
+    displayAllUserDetails(); // Display all user details
+    alert('User details stored successfully!');
+  });
+
+  // Function to display all user details
+  function displayAllUserDetails() {
+    var displayDiv = document.getElementById('userDetailsDisplay');
+    var allUsers = JSON.parse(localStorage.getItem('users')) || [];
+
+    // Clear the display before updating with new details
+    displayDiv.innerHTML = '';
+
+    if (allUsers.length > 0) {
+      allUsers.forEach(function(userDetails, index) {
+        var userDiv = document.createElement('div');
+        userDiv.innerHTML = `
+          <h2>User Details:</h2>
+          <p>Username: ${userDetails.username}</p>
+          <p>Email: ${userDetails.email}</p>
+          <p>Phone: ${userDetails.phone}</p>
+          <button onclick="deleteUser(${index})">Delete</button>
+          <hr>
+        `;
+        displayDiv.appendChild(userDiv);
+      });
+    } else {
+      displayDiv.innerHTML = '<p>No user details found.</p>';
+    }
+  }
+
+  // Function to delete user by index
+  function deleteUser(index) {
+    var allUsers = JSON.parse(localStorage.getItem('users')) || [];
+    allUsers.splice(index, 1); // Remove user at the specified index
+    localStorage.setItem('users', JSON.stringify(allUsers)); // Update local storage
+    displayAllUserDetails(); // Update the display
+  }
+
+  // Display all user details on page load
+  displayAllUserDetails();
