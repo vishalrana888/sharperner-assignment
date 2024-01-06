9b856b941caf6491ddc3d2fa67cc6a9097da9f00
vishalrana888 <vishalrana888@gmail.com>
2024-01-06 14:17:47 +0530

    async/promise

--- node/play.js
+++ node/play.js
@@ -18,10 +18,30 @@
 
 // array
 
-const a =['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
-let transform_a=(a.map(fruit => fruit === ' ' ? 'empty string' : fruit));
+// const a =['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+// let transform_a=(a.map(fruit => fruit === ' ' ? 'empty string' : fruit));
 
-// console.log(a);
-// console.log(transform_a);
-a.push("banana");
-console.log(a)
+// // console.log(a);
+// // console.log(transform_a);
+// a.push("banana");
+// console.log(a)
+
+
+// async/Promise
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const asyncFunction = async () => {
+  console.log('a');
+  console.log('b');
+  
+   await delay(3000);
+  console.log('c');
+  
+  await delay(0);
+  console.log('d');
+
+  console.log('e');
+};
+
+asyncFunction()
