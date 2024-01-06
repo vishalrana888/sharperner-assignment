f849ec0c4bf673eaa91f71b7c5be5e2b18be3a17
vishalrana888 <vishalrana888@gmail.com>
2024-01-06 12:38:48 +0530

    map and push method

--- node/play.js
+++ node/play.js
@@ -1,17 +1,27 @@
 // an arrow function which returns the product of two numbers
 
-const product =(a,b)=>{
-    return (a*b) ;
-}
-console.log(product(4,5));
+// const product =(a,b)=>{
+//     return (a*b) ;
+// }
+// console.log(product(4,5));
 
-// student object
+// // student object
 
-const student ={
-    name: "vishal",
-    roll_no: 76,
-    greet() {
-       console.log('Hi, Iam ' + this.name);
-    }
-};
-student.greet();
+// const student ={
+//     name: "vishal",
+//     roll_no: 76,
+//     greet() {
+//        console.log('Hi, Iam ' + this.name);
+//     }
+// };
+// student.greet();
+
+// array
+
+const a =['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+let transform_a=(a.map(fruit => fruit === ' ' ? 'empty string' : fruit));
+
+// console.log(a);
+// console.log(transform_a);
+a.push("banana");
+console.log(a)
