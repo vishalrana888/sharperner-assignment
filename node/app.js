71d41b4698b3b109b93dc62e17542b768862c95f
vishalrana888 <vishalrana888@gmail.com>
2024-01-12 10:33:40 +0530

    routes and filter

--- node/app.js
+++ node/app.js
@@ -3,29 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST">' +
-    '<input type="text" name="title" placeholder="Product Title">' +
-    '<input type="text" name="size" placeholder="Product Size">' +
-    '<button type="submit">Add Product</button></form>');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-
-app.post('/product', (req, res, next) => {
-  const title = req.body.title;
-  const size = req.body.size;
-
-  console.log('Product Title:', title);
-  console.log('Product Size:', size);
-
-  res.redirect('/');
-});
-
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
