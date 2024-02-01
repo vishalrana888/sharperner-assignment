fe44919bcb602739a846bc78aa288c624656fd6d
vishalrana888 <vishalrana888@gmail.com>
2024-02-01 18:49:10 +0530

    modal and file read and write

--- node/controllers/products.js
+++ node/controllers/products.js
@@ -17,7 +17,7 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
+  const products = Product.fetchAll(products => {
   res.render('shop', {
     prods: products,
     pageTitle: 'Shop',
@@ -26,4 +26,5 @@ exports.getProducts = (req, res, next) => {
     activeShop: true,
     productCSS: true
   });
+});
 };
